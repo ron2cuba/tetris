6655ca2e80e238fb1b5146019f54dffa2438f84f
ron2cuba <ronan.lenouvel@gmail.com>
2021-04-08 10:43:15 +0200

    ready to start

---
+++ js/app.js
@@ -0,0 +1,4 @@
+let ctx = document.querySelector("#canvas").getContext('2d');
+ctx.width = 500;
+ctx.height = 500;
+console.log(ctx.height);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tetris
+1) Préparation du canvas et log du javascript
+2) les pieces
